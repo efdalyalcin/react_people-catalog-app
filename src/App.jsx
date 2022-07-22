500271d5704cbdbf0074f58e8514d0ae59a416fd
efdalyalcin <efdalyalcin@hotmail.com>
2022-07-22 18:21:50 +0300

    App is done except the avatar changing with name change problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { Users } from './components/Users/Users';
-import './App.scss';
-import { UserEdit } from './components/UserEdit/UserEdit';
 
 export const App = () => {
   return (
     <div className="App">
       <Users />
-      {/* <UserEdit /> */}
     </div>
   );
 }
